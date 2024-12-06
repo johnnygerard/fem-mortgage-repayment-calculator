b2bc1ab1f1e6417b5fb572e980ba048ae1f9ff63
Johnny Gérard <john@jgerard.dev>
2024-12-06 23:02:33 +0100

    feat: add mortgage-limits.ts

---
+++ src/app/constants/mortgage-limits.ts
@@ -0,0 +1,8 @@
+export const MIN_PRINCIPAL = 1_000;
+export const MAX_PRINCIPAL = 1_000_000;
+
+export const MIN_TERM = 1;
+export const MAX_TERM = 100;
+
+export const MIN_INTEREST_RATE = 0.1;
+export const MAX_INTEREST_RATE = 10;
