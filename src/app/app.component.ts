e91d07a276dbc0d487d922eb65da3ad3859573d1
Johnny Gérard <john@jgerard.dev>
2024-12-06 22:04:41 +0100

    feat: reset root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   imports: [],
   templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = "ng-fem-starter";
-}
+export class AppComponent {}
