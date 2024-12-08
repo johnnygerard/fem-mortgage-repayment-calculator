308dd3fb30938f3449a32636f969f0d0dcaf1b5c
Johnny Gérard <john@jgerard.dev>
2024-12-08 09:58:53 -0500

    feat: add NumberFieldComponent (#6)

---
+++ src/app/components/number-field/number-field.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, input } from "@angular/core";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
+
+@Component({
+  selector: "app-number-field",
+  imports: [ReactiveFormsModule],
+  templateUrl: "./number-field.component.html",
+  host: { class: "block" },
+  styles: `
+    /* Hide number input arrows */
+    input[type="number"] {
+      &::-webkit-outer-spin-button,
+      &::-webkit-inner-spin-button {
+        display: none;
+      }
+
+      /* Firefox */
+      appearance: textfield;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NumberFieldComponent {
+  control = input.required<FormControl>();
+  label = input.required<string>();
+  unit = input.required<{
+    symbol: string;
+    position: "left" | "right";
+  }>();
+}
