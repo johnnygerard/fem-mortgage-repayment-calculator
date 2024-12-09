93f2b7cea034f366eae6b5b7788e610eff41aea7
Johnny Gérard <john@jgerard.dev>
2024-12-09 17:41:14 +0100

    feat: center results in desktop layout

--- src/app/components/mortgage-results/mortgage-results.component.ts
+++ src/app/components/mortgage-results/mortgage-results.component.ts
@@ -8,7 +8,7 @@ import { MortgageService } from "../../services/mortgage.service";
   templateUrl: "./mortgage-results.component.html",
   host: {
     class:
-      "text-preset-4 text-slate-300 block bg-slate-900 py-400 px-300 tb:p-500 dt:rounded-bl-[5rem]",
+      "text-preset-4 text-slate-300 block bg-slate-900 py-400 px-300 tb:p-500 dt:rounded-bl-[5rem] dt:grid dt:place-content-center",
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
