7156ce0cf6427eb390a5a038954cf3fdfc8fce53
Johnny Gérard <john@jgerard.dev>
2024-12-09 17:38:46 +0100

    feat: add app-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { NonNullableFormBuilder, Validators } from "@angular/forms";
+import { MortgageCalculatorComponent } from "./components/mortgage-calculator/mortgage-calculator.component";
+import { MortgageResultsComponent } from "./components/mortgage-results/mortgage-results.component";
 
 @Component({
   selector: "app-root",
-  imports: [],
+  imports: [MortgageCalculatorComponent, MortgageResultsComponent],
   templateUrl: "./app.component.html",
-  styles: `
-    :host {
-      display: block;
-    }
-  `,
+  host: {
+    class:
+      "flex flex-col tb:rounded-[1.5rem] overflow-hidden dt:flex-row bg-white tb:max-tb:w-[43rem]",
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  form = inject(NonNullableFormBuilder).group({
+    principal: ["", [Validators.required]],
+    term: ["", [Validators.required]],
+    interestRate: ["", [Validators.required]],
+    chargeType: ["", [Validators.required]],
+  });
+}
