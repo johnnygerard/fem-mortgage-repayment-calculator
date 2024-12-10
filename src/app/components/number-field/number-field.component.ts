75dd82c97133c89567cec928ebe6fec784257cee
Johnny Gérard <john@jgerard.dev>
2024-12-10 21:43:03 +0100

    feat: add style for active state

--- src/app/components/number-field/number-field.component.ts
+++ src/app/components/number-field/number-field.component.ts
@@ -1,5 +1,11 @@
 import { animate, style, transition, trigger } from "@angular/animations";
-import { ChangeDetectionStrategy, Component, input } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  OnDestroy,
+  signal,
+} from "@angular/core";
 import { FormControl, ReactiveFormsModule } from "@angular/forms";
 import { ErrorPipe } from "../../pipes/error.pipe";
 import { showValidationErrors } from "../../utils/show-validation-errors";
@@ -28,20 +34,31 @@ import { showValidationErrors } from "../../utils/show-validation-errors";
     ]),
   ],
 })
-export class NumberFieldComponent {
+export class NumberFieldComponent implements OnDestroy {
   control = input.required<FormControl>();
   label = input.required<string>();
   max = input.required<number>();
   min = input.required<number>();
   step = input.required<number>();
   isFormSubmitted = input.required<boolean>();
-
+  isActive = signal(false);
   unit = input.required<{
     symbol: string;
     position: "left" | "right";
   }>();
+  #timeoutId = 0;
 
   get showErrors(): boolean {
     return showValidationErrors(this.control(), this.isFormSubmitted());
+  }
+
+  refreshActiveState(): void {
+    clearTimeout(this.#timeoutId);
+    this.isActive.set(true);
+    this.#timeoutId = window.setTimeout(() => this.isActive.set(false), 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.#timeoutId);
   }
 }
